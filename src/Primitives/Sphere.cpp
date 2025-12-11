77bf42c7aa7b1c918909a17f13396bdefa82f62e
Aliff Ezaqiel Zaffrulla <aezaqiel@gmail.com>
2025-12-12 01:40:18 +0800

    Sphere intersection refactor

--- src/Primitives/Sphere.cpp
+++ src/Primitives/Sphere.cpp
@@ -1,7 +1,5 @@
 #include "Sphere.hpp"
 
-#include <glm/gtx/intersect.hpp>
-
 Sphere::Sphere(const glm::vec3& center, f32 radius)
     : m_Center(center), m_Radius(radius)
 {
@@ -9,21 +7,32 @@ Sphere::Sphere(const glm::vec3& center, f32 radius)
 
 bool Sphere::Hit(const Ray& ray, Interval t, HitRecord& record) const
 {
-    glm::vec3 intersection;
-    glm::vec3 normal;
+    glm::vec3 oc = ray.origin - m_Center;
 
-    if (!glm::intersectRaySphere(ray.origin, glm::normalize(ray.direction), m_Center, m_Radius, intersection, normal)) {
+    f32 a = glm::dot(ray.direction, ray.direction);
+    f32 halfB = glm::dot(oc, ray.direction);
+    f32 c = glm::dot(oc, oc) - m_Radius * m_Radius;
+
+    f32 discriminant = halfB * halfB - a * c;
+
+    if (discriminant < 0.0f) {
         return false;
     }
 
-    f32 distance = glm::distance(ray.origin, intersection);
-    if (!t.surrounds(distance)) {
-        return false;
+    f32 sqrtd = glm::sqrt(discriminant);
+
+    f32 root = (-halfB - sqrtd) / a;
+
+    if (!t.surrounds(root)) {
+        root = (-halfB + sqrtd) / a;
+        if (!t.surrounds(root)) {
+            return false;
+        }
     }
 
-    record.p = intersection;
-    record.t = distance;
-    record.SetFace(ray, normal);
+    record.t = root;
+    record.p = ray.At(root);
+    record.SetFace(ray, (record.p - m_Center) / m_Radius);
 
     return true;
 }
